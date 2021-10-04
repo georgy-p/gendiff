8f6fb76d381b0c767f141be9ba90ba8667b58f87
George <pigargeorge@gmail.com>
2021-10-04 15:30:36 +0300

    fixed problem with sorting and add getSorted

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import _ from 'lodash';
-import { getData } from './src/filetools.js';
+// eslint-disable-next-line import/extensions
+import getData from './src/filetools.js';
 
+const getSorted = (coll) => coll.sort((a, b) => {
+  if (a[1] === b[1]) {
+    if (a[0].includes('-')) {
+      return -1;
+    }
+  } else if (a[1] > b[1]) {
+    return 1;
+  } else if (a[1] < b[1]) {
+    return -1;
+  }
+  return 0;
+});
 
-export const genDiff = (fileName1, fileName2) => {
+export default (fileName1, fileName2) => {
   const file1 = JSON.parse(getData(fileName1));
   const file2 = JSON.parse(getData(fileName2));
   const file1Keys = Object.keys(file1);
@@ -13,20 +25,20 @@ export const genDiff = (fileName1, fileName2) => {
   const acc = [];
   commonKeys.map((key) => {
     if (file1[key] === file2[key]) {
-      acc.push([' ', `${key}: ${file1[key]}\n`]);
+      acc.push(['    ', `${key}:`, `${file1[key]}\n`]);
     } else {
-      acc.push(['-', `${key}: ${file1[key]}\n`]);
-      acc.push(['+', `${key}: ${file2[key]}\n`]);
+      acc.push(['   -', `${key}:`, `${file1[key]}\n`]);
+      acc.push(['   +', `${key}:`, `${file2[key]}\n`]);
     }
-  }); 
+    return acc;
+  });
   uniq1Keys.forEach((key) => {
-    acc.push(['-', `${key}: ${file1[key]}\n`]);
+    acc.push(['   -', `${key}:`, `${file1[key]}\n`]);
   });
   uniq2Keys.forEach((key) => {
-    acc.push(['+', `${key}: ${file2[key]}\n`]);
+    acc.push(['   +', `${key}:`, `${file2[key]}\n`]);
   });
-  acc.sort((a, b) => a[1].localeCompare(b[1]));
-  const result = `{\n ${acc.flat().join(' ')}}`;
-  console.log(result);
+  const sortedAcc = getSorted(acc);
+  const result = `{\n ${sortedAcc.flat().join(' ')}  }`;
   return result;
 };
