e31841f77394ab81fc5d8551875c118948576c2c
George <pigargeorge@gmail.com>
2021-10-08 20:58:10 +0300

    update test for deep diff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,19 +9,12 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 let result;
 
-const file1 = 'test_file1.json';
-const file2 = 'test_file2.json';
-const fullPath = getFixturePath(file1);
+const file1 = 'file1.json';
+const file2 = 'file2.json';
+const fullPath = getFixturePath('file1');
 
 beforeEach(() => {
-  result = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  }`;
+  result = getFixturePath('result.yml');
 });
 
 test('genDiff test1: short name', () => {
@@ -32,8 +25,8 @@ test('genDiff test2: full path', () => {
   expect(genDiff(fullPath, file2)).toEqual(result);
 });
 
-const ymlFile1 = 'test_file1.yml';
-const ymlFile2 = 'test_file2.yml';
+const ymlFile1 = 'filepath1.yml';
+const ymlFile2 = 'filepath2.yml';
 
 test('genDiff test3: yml files compare', () => {
   expect(genDiff(ymlFile1, ymlFile2)).toEqual(result);
