f243f0bf887f7f9bcf94d14a9aa68a21856c4ce4
George <pigargeorge@gmail.com>
2021-10-15 10:31:04 +0300

    add default format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-// eslint-disable-next-line import/extensions
 import genDiff from '../index.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', stylish)
   .action((file1, file2) => console.log(genDiff(file1, file2)));
 
 program.parse();
