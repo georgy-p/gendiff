269eab27c5eaf158a2e5d1e535487f1d725c780c
George <pigargeorge@gmail.com>
2021-10-27 18:04:47 +0300

    fix ast.js remove getClone function

--- src/ast.js
+++ src/ast.js
@@ -1,25 +1,22 @@
 import _ from 'lodash';
 
-const getClone = (data) => {
-  if (!_.isPlainObject(data)) {
-    return data;
-  }
-  const result = {};
-  const keys = Object.keys(data);
-  keys.map((key) => {
-    if (_.isPlainObject(data[key])) {
-      result[`  ${key}`] = getClone(data[key]);
-    } else {
-      result[`  ${key}`] = data[key];
-    }
-    return result;
-  });
-  return result;
-};
-
 const buildAst = (data1, data2) => {
   const unionKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
   const result = unionKeys.map((key) => {
+    if (!_.has(data2, key)) {
+      return {
+        name: key,
+        type: 'removed',
+        value: _.cloneDeep(data1[key]),
+      };
+    }
+    if (!_.has(data1, key)) {
+      return {
+        name: key,
+        type: 'added',
+        value: _.cloneDeep(data2[key]),
+      };
+    }
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return {
         name: key,
@@ -27,32 +24,19 @@ const buildAst = (data1, data2) => {
         children: buildAst(data1[key], data2[key]),
       };
     }
-    if (!_.has(data2, key)) {
+    if (_.isEqual(data1[key], data2[key])) {
       return {
-        name: key,
-        type: 'removed',
-        value: getClone(data1[key]),
-      };
-    }
-    if (!_.has(data1, key)) {
-      return {
-        name: key,
-        type: 'added',
-        value: getClone(data2[key]),
-      };
-    }
-    return (_.isEqual(data1[key], data2[key]))
-      ? {
         name: key,
         type: 'unchanged',
-        value: getClone(data1[key]),
-      }
-      : {
-        name: key,
-        type: 'updated',
-        oldValue: getClone(data1[key]),
-        newValue: getClone(data2[key]),
+        value: _.cloneDeep(data1[key]),
       };
+    }
+    return {
+      name: key,
+      type: 'updated',
+      oldValue: _.cloneDeep(data1[key]),
+      newValue: _.cloneDeep(data2[key]),
+    };
   });
   return result;
 };
